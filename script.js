0fa214c18aca22bce31888e101ffb6594ace2982
fajar putra gumilang <fajarputragumilangpg@gmail.com>
2024-08-21 21:55:10 +0700

    update

--- script.js
+++ script.js
@@ -12,11 +12,54 @@ const hamburger = document.querySelector("#hamburger");
 const navbar = document.querySelector("#navbar");
 
 
-window.onload = function() {
-    setTimeout(function() {
-        document.getElementById("loader").classList.add('loading')
-    }, 2000);
+
+
+// window.addEventListener("load", function () {
+//    // Hide the loader after the page has fully loaded
+//     setTimeout(() => {
+//     document.getElementById("loader").classList.add('loading');
+//     bulat.forEach(function (bulat) {
+//         bulat.classList.add('hidden')
+//     });
+//     },3000)
+// });
+
+// function shouldShowLoader() {
+//             return !localStorage.getItem('loaded');
+//         }
+
+//         // If the page was refreshed, show the loader
+//         if (shouldShowLoader()) {
+//             document.getElementById('loader').classList.remove('hidden');
+//             window.addEventListener("load", function() {
+//                 setTimeout(() => {
+//                     document.getElementById("loader").classList.add('hidden');
+//                     localStorage.setItem("loaded", true); 
+//                 },2000);
+                  
+//             });
+//         } else {
+//             // If navigating internally, skip the loader
+//                 document.getElementById("loader").classList.add('hidden')
+//         }
+
+//         // Reset loader status on refresh
+//         window.addEventListener("beforeunload", function() {
+//             localStorage.removeItem('loaded');
+//         });
+
+//         // Optional: Handle 'Home' button click if it's an internal link
+//         document.querySelector('#home').addEventListener('click', function() {
+//             localStorage.setItem('loaded', true);
+//         });
+
+
+function hideLoader(e) {
+    setTimeout(() => { 
+        document.getElementById("loader").classList.add("hidden");
+    }, 3000);
 }
+hideLoader();
 
 
 
@@ -27,10 +70,10 @@ hamburger.addEventListener("click", function () {
     hamburger.classList.toggle('hamburger-active');
 });
 
-document.addEventListener('click', function(e) {
-    if(e.target.closest('#hamburger') != e.target.closest('#navbar')) return 
-        navbar.classList.add('hidden');
-        hamburger.classList.remove('hamburger-active')
+document.addEventListener('click', function (e) {
+    if (e.target.closest('#hamburger') != e.target.closest('#navbar')) return
+    navbar.classList.add('hidden');
+    hamburger.classList.remove('hamburger-active')
 })
 
 
